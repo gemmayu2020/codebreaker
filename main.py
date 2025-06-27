60e2a50a66ff7ae3af1ebc03809179af2976713f
Gemma Yu <gemma.yu2020@gmail.com>
2025-06-27 21:36:29 +0100

    encode message

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def create_code():
   
     for i in range(len(alphabet)):
         code[alphabet[i]] = backwards[i]
-    print(code)
+    #print(code)
 # Calculate the frequency of all letters in a piece of text
 
 
@@ -22,19 +22,41 @@ def create_code():
 
 
 # Encode/decode a piece of text — atbash is symetrical
-
+def atbash(text):
+    text = text.lower()
+    output=""
+    
+    for letter in text:
+        if letter in code:
+            output +=code[letter]
+    return output
 
 
 # Fetch and return text from a file
+def get_text(filename):
+    with open (filename) as f:
+        text = f.read().replace("/n","")
 
+    return text
 
 
 # Create a text-based menu system  
+def menu():
+    choice = " "
 
+    while choice != "c" and choice != "f":
+        choice = input("Enter c to encode/decode some text or enter f to perfom some frequency analysis: ")
 
-
+    if choice == "c":
+        print("Running message from cypher....")
+        message=get_text("input.txt")
+        ""
+        code=atbash(message)
+        print(code)
 # Start up
 def main():
     create_code()
+    #print(atbash("Test"))
+    menu()
 
 main()
